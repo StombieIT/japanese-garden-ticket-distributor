55067c9ac0ea7088d6b316051e5683c424254e49
Vladislav Yartsev <vladislav.yartsev@vk.team>
2024-05-18 19:35:01 +0300

    fix account

--- client/src/components/Account/Account.tsx
+++ client/src/components/Account/Account.tsx
@@ -8,16 +8,7 @@ import classes from "./Account.module.styl";
 import {ProfileCard} from "@/components/ProfileCard/ProfileCard";
 import {$authUserStore} from "@/state-management/auth";
 import {Button} from "@/components/Button/Button";
-import {PassageTicket} from "@/components/PassageTicket/PassageTicket";
-import {IPassage} from "@/models/passage";
 import {PassageTickets} from "@/components/PassageTickets/PassageTickets";
-
-const passage: IPassage = {
-    id: 1,
-    date: "30.10.2003",
-    entryTime: "17:00",
-    status: null
-};
 
 export const Account: FC = () => {
     const authUser = useUnit($authUserStore);
