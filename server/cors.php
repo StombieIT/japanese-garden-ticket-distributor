9aca0077b57e08c753320d2d91dd8675a1a9bb48
Vladislav Yartsev <vladislav.yartsev@vk.team>
2024-05-24 22:01:46 +0300

    delete cors comments

--- server/cors.php
+++ server/cors.php
@@ -1,7 +1,5 @@
 <?php
     header('Access-Control-Allow-Origin: http://localhost:5173');
     header("Access-Control-Allow-Headers: *");
-//    header("Access-Control-Allow-Headers: Content-Type, X-Requested-With");
     header("Access-Control-Allow-Credentials: true");
-//    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 ?>
